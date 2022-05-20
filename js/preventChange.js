a29afa8320b398fc0d43525a3bb4e16c474cd268
cph101 <cph101@icloud.com>
2022-05-20 19:11:46 +0100

    Update preventChange.js

--- js/preventChange.js
+++ js/preventChange.js
@@ -1,10 +1,16 @@
 const originalContents = document.body.innerHTML;
+const originalHead = document.head.innerHTML;
 
 function checkEdit() {
   if (document.body.innerHTML == originalContents) {
     // Everything is alright
   } else {
     document.body.innerHTML = originalContents;
+  }
+  if (document.head.innerHTML == originalHead) {
+    // Everything is alright
+  } else {
+    document.head.innerHTML = originalHead;
   }
 }
 
